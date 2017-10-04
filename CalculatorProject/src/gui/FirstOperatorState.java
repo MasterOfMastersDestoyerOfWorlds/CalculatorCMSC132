547deb1fc65f250d2db09a6fd7e7f6a4d84f7c13
awollac <awollac@wireless-10-105-224-154.umd.edu>
2017-10-03 21:51:44 -0400

    added the framework for state implementation and all the buttons

---
+++ CalculatorProject/src/gui/FirstOperatorState.java
@@ -0,0 +1,11 @@
+package gui;
+
+public class FirstOperatorState implements State{
+
+	@Override
+	public void addOperator(String operator) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ CalculatorProject/src/gui/State.java
@@ -0,0 +1,5 @@
+package gui;
+
+public interface State {
+	public void addOperator(String operator);
+}
