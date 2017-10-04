e3168c598274ec6c6c0bf8b91658f986224c3da2
awollac <awollac@wireless-10-105-224-154.umd.edu>
2017-10-04 14:34:33 -0400

    added error state

---
+++ CalculatorProject/src/gui/ErrorState.java
@@ -0,0 +1,11 @@
+package gui;
+
+public class ErrorState implements State {
+
+	@Override
+	public void addOperator(String operator, CalculatorGUI calculatorGUI) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
